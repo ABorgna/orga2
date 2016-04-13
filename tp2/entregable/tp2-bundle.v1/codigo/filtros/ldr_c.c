11b1c18a51c36d8ddfce512b557ff65b0f65995b
Agustin Borgna <hello@aborgna.com.ar>
2016-04-13 15:42:24 -0300

    ldr en C

--- tp2/entregable/tp2-bundle.v1/codigo/filtros/ldr_c.c
+++ tp2/entregable/tp2-bundle.v1/codigo/filtros/ldr_c.c
@@ -6,27 +6,44 @@
 
 #define P 2
 
-void ldr_c    (
+void ldr_c (
     unsigned char *src,
     unsigned char *dst,
     int cols,
     int filas,
     int src_row_size,
     int dst_row_size,
-	int alpha)
+    int alpha)
 {
-    unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
-    unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
+    const unsigned int LDR_MAX = 5 * 5 * 255 * 3 * 255;
 
-    for (int i = 0; i < filas; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            bgra_t *p_d = (bgra_t*) &dst_matrix[i][j * 4];
-            bgra_t *p_s = (bgra_t*) &src_matrix[i][j * 4];
-            *p_d = *p_s;
+    bgra_t (*src_matrix)[src_row_size] = (bgra_t(*)[src_row_size]) src;
+    bgra_t (*dst_matrix)[dst_row_size] = (bgra_t(*)[dst_row_size]) dst;
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < cols; j++) {
+            bgra_t pixel = src_matrix[i][j * 4];
+
+            if(i >= 2 && i < filas-2 && j >= 2 && j < cols-2) {
+                unsigned int acum = 0;
+
+                for(char di = -2; di <= 2; di++) {
+                    for(char dj = -2; dj <= 2; dj++) {
+                        bgra_t neightbor = src_matrix[i+di][(j+dj)*4];
+
+                        acum += neightbor.r;
+                        acum += neightbor.g;
+                        acum += neightbor.b;
+                    }
+                }
+
+                pixel.r += (alpha * acum * pixel.r) / LDR_MAX;
+                pixel.g += (alpha * acum * pixel.g) / LDR_MAX;
+                pixel.b += (alpha * acum * pixel.b) / LDR_MAX;
+            }
+
+            dst_matrix[i][j * 4] = pixel;
         }
     }
 }
-
 
