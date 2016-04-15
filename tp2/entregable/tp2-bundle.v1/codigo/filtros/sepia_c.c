678eab89e7e25fadeef202ba13816e6e0b595223
Alan Corleto <corletoalan@gmail.com>
2016-04-15 11:35:19 -0300

    La implementación en C de sepia

--- tp2/entregable/tp2-bundle.v1/codigo/filtros/sepia_c.c
+++ tp2/entregable/tp2-bundle.v1/codigo/filtros/sepia_c.c
@@ -10,18 +10,29 @@ void sepia_c    (
     int src_row_size,
     int dst_row_size)
 {
-    unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
-    unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
+    //unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
+    //unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
+
+    bgra_t (*src_matrix)[cols] = (bgra_t(*)[cols]) src;
+    bgra_t (*dst_matrix)[cols] = (bgra_t(*)[cols]) dst;
 
     for (int i = 0; i < filas; i++)
     {
         for (int j = 0; j < cols; j++)
         {
-            bgra_t *p_d = (bgra_t*) &dst_matrix[i][j * 4];
-            bgra_t *p_s = (bgra_t*) &src_matrix[i][j * 4];
-            *p_d = *p_s;
+            bgra_t pixel = src_matrix[i][j];
+            unsigned short suma = pixel.r + pixel.g + pixel.b;
+            unsigned short suma_r = 0.5*suma;
+            if(suma_r < 255)                                    //Este número puede dar más de 255. Por eso le calculo el mínimo
+                pixel.r = suma_r;                       
+            else
+                pixel.r = 255;
+            pixel.g = 0.3*suma;                                 //Este no
+            pixel.b = 0.2*suma;                                 //Este menos
+            
+            dst_matrix[i][j] = pixel;
         }
-    }	//COMPLETAR
+    }   //COMPLETAR
 }
 
 
