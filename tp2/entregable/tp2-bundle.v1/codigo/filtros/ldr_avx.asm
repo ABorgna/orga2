85fe7c8c19ace2bebb552bbf21026c6eaaac3673
Agustin Borgna <hello@aborgna.com.ar>
2016-04-27 13:47:02 -0300

    ldr_avx working

--- tp2/entregable/tp2-bundle.v1/codigo/filtros/ldr_avx.asm
+++ tp2/entregable/tp2-bundle.v1/codigo/filtros/ldr_avx.asm
@@ -21,8 +21,8 @@ section .text
 ;void ldr_asm    (
     ; rdi | unsigned char *src,
     ; rsi | unsigned char *dst,
-    ; edx | int filas,
-    ; ecx | int cols,
+    ; edx | int cols,
+    ; ecx | int filas,
     ; r8d | int src_row_size,
     ; r9d | int dst_row_size,
     ; bp+16 | int alpha
@@ -55,7 +55,8 @@ ldr_avx:
     ; r10 = alpha
     ; rdi = current pixel src
     ; rsi = current pixel dst
-    mov rax, rcx
+    mov rax, rdx
+    mov rdx, rcx
     mov r8, rdi
     mov r9, rsi
     movsx r10, word [rbp+16]
@@ -67,9 +68,9 @@ ldr_avx:
     ; xmm15 is always zero
     vzeroall
 
-    ; mm0 = | LDR_MAX_INV. | LDR_MAX_INV. |
-    ; mm1 = |     255.     |     255.     |
-    ; mm2 = |    alpha.    |    alpha.    |
+    ; mm0 = |   LDR_MAX_MAGIC.  |   LDR_MAX_MAGIC   |
+    ; mm1 = |        255.       |        255        |
+    ; mm2 = |       alpha.      |       alpha.      |
     movq mm0, [LDR_MAX_INV]
     movq mm1, [PIXEL_MAX_F]
     pinsrd xmm6, r10d, 0
@@ -78,8 +79,8 @@ ldr_avx:
     movdq2q mm2, xmm6
 
     ; Empezamos a procesar desde fila2 - 4px
-    lea rsi, [r8 + r15 - 16]
-    lea rdi, [r9 + r15 - 16]
+    lea rsi, [r8 + r15 - 8]
+    lea rdi, [r9 + r15 - 8]
 
     ; Magic loop, correr sobre ((filas-4)*cols+4)/4
     lea rcx, [rdx-4]    ; rcx = filas-4
@@ -89,26 +90,26 @@ ldr_avx:
 
     .magicLoop:
 
-        ; xmmL tiene la suma de los primeros 4 pixeles vecinos y la parte alta en 0
-; |    0    |    0    |    0    |    0    |   sum3  |   sum2  |   sum1  |   sum0  | xmmL
+        ; xmm0 tiene la suma de las primeras cuatro columnas que necesitamos y lo demas en 0
+; |    0    |    0    |    0    |    0    |sum_col3 |sum_col2 |sum_col1 |sum_col0 | xmm0
 
         ; load the new pixels
         ; ->
 ; | A7 | R7 | G7 | B7 | A6 | R6 | G6 | B6 | A5 | R5 | G5 | B5 | A4 | R4 | G4 | B4 | xmmN
-        movdqu xmm5, [rsi+r12+16]
-        movdqu xmm6, [rsi+r13+16]
-        movdqu xmm7, [rsi+16]
-        movdqu xmm8, [rsi+r14+16]
-        movdqu xmm9, [rsi+r15+16]
+        vmovdqu xmm5, [rsi+r12+8]
+        vmovdqu xmm6, [rsi+r13+8]
+        vmovdqu xmm7, [rsi+8]
+        vmovdqu xmm8, [rsi+r14+8]
+        vmovdqu xmm9, [rsi+r15+8]
 
         ; shift to clear the alpha bits
         ; ->
 ; | R7 | G7 | B7 |  0 | R6 | G6 | B6 |  0 | R5 | G5 | B5 |  0 | R4 | G4 | B4 |  0 | xmmN
-        pslld xmm5, 8
-        pslld xmm6, 8
-        pslld xmm7, 8
-        pslld xmm8, 8
-        pslld xmm9, 8
+        vpslld xmm5, 8
+        vpslld xmm6, 8
+        vpslld xmm7, 8
+        vpslld xmm8, 8
+        vpslld xmm9, 8
 
         ; unpack bytes to words, xmmN -> xmm{N+5}:xmmN
         ; ->
@@ -134,96 +135,62 @@ ldr_avx:
         vphaddw xmm8, xmm8, xmm13
         vphaddw xmm9, xmm9, xmm14
         ; ->
-;| R7+G7+B7| R6+G6+B6| R5+G5+B5| R4+G4+B4|    0    |    0    |    0    |    0     | xmmN
-;|   sum7  |   sum6  |   sum5  |   sum4  |    0    |    0    |    0    |    0     | xmmN
-        vphaddw xmm5, xmm15, xmm5
-        vphaddw xmm6, xmm15, xmm6
-        vphaddw xmm7, xmm15, xmm7
-        vphaddw xmm8, xmm15, xmm8
-        vphaddw xmm9, xmm15, xmm9
-
-        ; Get the final sums
-        ; ->
-;|   sum7  |   sum6  |   sum5  |   sum4  |   sum3  |   sum2  |   sum1  |   sum0  | xmmN
-        por xmm5, xmm0
-        por xmm6, xmm1
-        por xmm7, xmm2
-        por xmm8, xmm3
-        por xmm9, xmm4
-
-        ; Calcular sumargb_i (por linea) para cada uno de los 4 pixeles que vamos a procesar
-        ; En el proceso vamos eliminando las sumas de pixeles viejos y dejamos xmmL
-        ; listos para la siguiente vuelta
-        ; ->
-;|    X    |    X    |    X    |    X    |sumargb3n|sumargb2n|sumargb1n|sumargb0n| xmmN
-;|    0    |    0    |    0    |    0    |   sum7  |   sum6  |   sum5  |   sum4  | xmmL
-        vpsrldq xmm0, xmm5, 2 ; Desechar valor viejo j-2
-        vpsrldq xmm1, xmm6, 2
-        vpsrldq xmm2, xmm7, 2
-        vpsrldq xmm3, xmm8, 2
-        vpsrldq xmm4, xmm9, 2
-        vpaddw xmm5, xmm5, xmm0 ; Sumar j-1
-        vpaddw xmm6, xmm6, xmm1
-        vpaddw xmm7, xmm7, xmm2
-        vpaddw xmm8, xmm8, xmm3
-        vpaddw xmm9, xmm9, xmm4
-        vpsrldq xmm0, xmm0, 2 ; Desechar valor viejo j-1
-        vpsrldq xmm1, xmm1, 2
-        vpsrldq xmm2, xmm2, 2
-        vpsrldq xmm3, xmm3, 2
-        vpsrldq xmm4, xmm4, 2
-        vpaddw xmm5, xmm5, xmm0 ; Sumar j
-        vpaddw xmm6, xmm6, xmm1
-        vpaddw xmm7, xmm7, xmm2
-        vpaddw xmm8, xmm8, xmm3
-        vpaddw xmm9, xmm9, xmm4
-        vpsrldq xmm0, xmm0, 2 ; Desechar valor viejo j
-        vpsrldq xmm1, xmm1, 2
-        vpsrldq xmm2, xmm2, 2
-        vpsrldq xmm3, xmm3, 2
-        vpsrldq xmm4, xmm4, 2
-        vpaddw xmm5, xmm5, xmm0 ; Sumar j+1
-        vpaddw xmm6, xmm6, xmm1
-        vpaddw xmm7, xmm7, xmm2
-        vpaddw xmm8, xmm8, xmm3
-        vpaddw xmm9, xmm9, xmm4
-        vpsrldq xmm0, xmm0, 2 ; Desechar valor viejo j+1
-        vpsrldq xmm1, xmm1, 2
-        vpsrldq xmm2, xmm2, 2
-        vpsrldq xmm3, xmm3, 2
-        vpsrldq xmm4, xmm4, 2
-        vpaddw xmm5, xmm5, xmm0 ; Sumar j+2
-        vpaddw xmm6, xmm6, xmm1
-        vpaddw xmm7, xmm7, xmm2
-        vpaddw xmm8, xmm8, xmm3
-        vpaddw xmm9, xmm9, xmm4
+;| R7+G7+B7| R6+G6+B6| R5+G5+B5| R4+G4+B4|    0    |    0    |    0    |    0     | xmm{N+5}
+        ; ==
+;|   sum7  |   sum6  |   sum5  |   sum4  |    0    |    0    |    0    |    0     | xmm{N+5}
+        vpxor xmm10, xmm10, xmm10
+        vpxor xmm11, xmm11, xmm11
+        vpxor xmm12, xmm12, xmm12
+        vpxor xmm13, xmm13, xmm13
+        vpxor xmm14, xmm14, xmm14
+        vphaddw xmm10, xmm10, xmm5
+        vphaddw xmm11, xmm11, xmm6
+        vphaddw xmm12, xmm12, xmm7
+        vphaddw xmm13, xmm13, xmm8
+        vphaddw xmm14, xmm14, xmm9
 
         ; Reducir todas las sumargb_i a una unica sumargb
         ; ->
+;|sum_col7 |sum_col6 |sum_col5 |sum_col4 |    0    |    0    |    0    |    0     | xmm10
+        vpaddw xmm13, xmm13, xmm14
+        vpaddw xmm10, xmm10, xmm11
+        vpaddw xmm12, xmm12, xmm13
+        vpaddw xmm10, xmm10, xmm12
+
+        ; Save the sums in xmm0
+        ; ->
+;|sum_col7 |sum_col6 |sum_col5 |sum_col4 |sum_col3 |sum_col2 |sum_col1 |sum_col0 | xmm0
+        vpor xmm0, xmm0, xmm10
+
+        ; Calcular sumargb para cada uno de los 4 pixeles que vamos a procesar
+        ; En el proceso vamos eliminando las sumas de pixeles viejos y dejamos xmm0
+        ; listo para la siguiente vuelta
+        ; ->
 ;|    X    |    X    |    X    |    X    |sumargb3 |sumargb2 |sumargb1 |sumargb0 | xmm5
-        paddw xmm8, xmm9
-        paddw xmm5, xmm6
-        paddw xmm7, xmm8
-        paddw xmm5, xmm7
+;|    0    |    0    |    0    |    0    |sum_col7 |sum_col6 |sum_col5 |sum_col4 | xmm0
+        vmovdqa xmm5, xmm0 ; Mover valor inicial, j-2
+        vpsrldq xmm0, xmm0, 2 ; Desechar valor viejo j-2
+        vpaddw xmm5, xmm5, xmm0 ; Sumar j-1
+        vpsrldq xmm0, xmm0, 2 ; Desechar valor viejo j-1
+        vpaddw xmm5, xmm5, xmm0 ; Sumar j
+        vpsrldq xmm0, xmm0, 2 ; Desechar valor viejo j
+        vpaddw xmm5, xmm5, xmm0 ; Sumar j+1
+        vpsrldq xmm0, xmm0, 2 ; Desechar valor viejo j+1
+        vpaddw xmm5, xmm5, xmm0 ; Sumar j+2
 
         ; Cargar alpha en xmm6 por cuadriplicado en fp
         ; ->
 ;|       alpha.      |       alpha.      |       alpha.      |       alpha.      | xmm6
+        vpxor xmm6, xmm6, xmm6
         movq2dq xmm6, mm2
-        movddup xmm6, xmm6
+        vmovddup xmm6, xmm6
 
         ; Expandir las sumas, convertirlas a fp y multiplicarlas por alpha
         ; ->
 ;| sumargb3 * alpha. | sumargb2 * alpha. | sumargb1 * alpha. | sumargb0 * alpha. | xmm5
-        punpcklwd xmm5, xmm15
-        cvtdq2ps xmm5, xmm5
-        mulps xmm5, xmm6
-
-        ; Cargamos el 1/max en xmm14 en fp
-        ; ->
-;|      1/MAX.       |      1/MAX.       |      1/MAX.       |      1/MAX.       | xmm14
-        movq2dq xmm14, mm0
-        movddup xmm14, xmm14
+        vpunpcklwd xmm5, xmm5, xmm15
+        vcvtdq2ps xmm5, xmm5
+        vmulps xmm5, xmm5, xmm6
 
         ; Movemos las sumas cada una replicada en un vector
         ; ->
@@ -231,79 +198,101 @@ ldr_avx:
 ;| sumargb2 * alpha. | sumargb2 * alpha. | sumargb2 * alpha. | sumargb2 * alpha. | xmm6
 ;| sumargb1 * alpha. | sumargb1 * alpha. | sumargb1 * alpha. | sumargb1 * alpha. | xmm7
 ;| sumargb0 * alpha. | sumargb0 * alpha. | sumargb0 * alpha. | sumargb0 * alpha. | xmm8
-        pshufd xmm8, xmm5, 0b00000000
-        pshufd xmm7, xmm5, 0b01010101
-        pshufd xmm6, xmm5, 0b10101010
-        pshufd xmm5, xmm5, 0b11111111
+        vpshufd xmm8, xmm5, 0b00000000
+        vpshufd xmm7, xmm5, 0b01010101
+        vpshufd xmm6, xmm5, 0b10101010
+        vpshufd xmm5, xmm5, 0b11111111
 
-        ; cargamos los valores de los pixeles a xmm9
+        ; Cargar los valores de los pixeles, unpackearlos a dw y pasarlos a float
         ; ->
-;| R3 | G3 | B3 |  0 | R2 | G2 | B2 |  0 | R1 | G1 | B1 |  0 | R0 | G0 | B0 |  0 | xmm9
+;| A3 | R3 | G3 | B3 | A2 | R2 | G2 | B2 | A1 | R1 | G1 | B1 | A0 | R0 | G0 | B0 | xmm9
         movdqu xmm9, [rsi]
-        pslld xmm9, 8 ; Clear the alpha bytes
-
-        ; Separar los valores de cada pixel, mandarlos a double word, convertirlos a fp
         ; ->
-;|    R3   |    G3   |   B3    |    0    |    R2   |    G2   |   B2    |    0    | xmm9
-;|    R1   |    G1   |   B1    |    0    |    R0   |    G0   |   B0    |    0    | xmm11
+;|    A3   |    R3   |    G3   |   B3    |    A2   |    R2   |    G2   |   B2    | xmm9
+;|    A1   |    R1   |    G1   |   B1    |    A0   |    R0   |    G0   |   B0    | xmm11
         vpunpcklbw xmm11, xmm9, xmm15
         vpunpckhbw xmm9, xmm9, xmm15
         ; ->
-;|         R3        |         G3        |         B3        |         0         | xmm9
-;|         R2        |         G2        |         B2        |         0         | xmm10
-;|         R1        |         G1        |         B1        |         0         | xmm11
-;|         R0        |         G0        |         B0        |         0         | xmm12
+;|        A3         |         R3        |         G3        |         B3        | xmm9
+;|        A2         |         R2        |         G2        |         B2        | xmm10
+;|        A1         |         R1        |         G1        |         B1        | xmm11
+;|        A0         |         R0        |         G0        |         B0        | xmm12
         vpunpcklwd xmm10, xmm9, xmm15
         vpunpckhwd xmm9, xmm9, xmm15
         vpunpcklwd xmm12, xmm11, xmm15
         vpunpckhwd xmm11, xmm11, xmm15
         ; ->
-;|         R3.       |         G3.       |         B3.       |         0.        | xmm9
-;|         R2.       |         G2.       |         B2.       |         0.        | xmm10
-;|         R1.       |         G1.       |         B1.       |         0.        | xmm11
-;|         R0.       |         G0.       |         B0.       |         0.        | xmm12
-        cvtdq2ps xmm9, xmm9
-        cvtdq2ps xmm10, xmm10
-        cvtdq2ps xmm11, xmm11
-        cvtdq2ps xmm12, xmm12
+;|        A3.        |         R3.       |         G3.       |         B3.       | xmm9
+;|        A2.        |         R2.       |         G2.       |         B2.       | xmm10
+;|        A1.        |         R1.       |         G1.       |         B1.       | xmm11
+;|        A0.        |         R0.       |         G0.       |         B0.       | xmm12
+        vcvtdq2ps xmm9, xmm9
+        vcvtdq2ps xmm10, xmm10
+        vcvtdq2ps xmm11, xmm11
+        vcvtdq2ps xmm12, xmm12
+
+        ; Cargar una mascara para borrar el canal alpha
+        ; ->
+;|         0         |    0xffffffff     |    0xffffffff     |    0xffffffff     | xmm13
+        vpxor xmm13, xmm13, xmm13 ; clean dependencies
+        vpcmpeqq xmm13, xmm13, xmm13
+        vpsrldq xmm13, xmm13, 4
 
         ; Multiplicamos cada valor de cada pixel por sumargb*alpha
+        ; y borramos el canal alpha
         ; ->
-;| R3*sumargb3*alpha.| G3*sumargb3*alpha.| B3*sumargb3*alpha.|         0.        | xmm5
-;| R2*sumargb2*alpha.| G2*sumargb2*alpha.| B2*sumargb2*alpha.|         0.        | xmm6
-;| R1*sumargb1*alpha.| G1*sumargb1*alpha.| B1*sumargb1*alpha.|         0.        | xmm7
-;| R0*sumargb0*alpha.| G0*sumargb0*alpha.| B0*sumargb0*alpha.|         0.        | xmm8
-        mulps xmm5, xmm9
-        mulps xmm6, xmm10
-        mulps xmm7, xmm11
-        mulps xmm8, xmm12
+;|         0.        | R3*sumargb3*alpha.| G3*sumargb3*alpha.| B3*sumargb3*alpha.| xmm5
+;|         0.        | R2*sumargb2*alpha.| G2*sumargb2*alpha.| B2*sumargb2*alpha.| xmm6
+;|         0.        | R1*sumargb1*alpha.| G1*sumargb1*alpha.| B1*sumargb1*alpha.| xmm7
+;|         0.        | R0*sumargb0*alpha.| G0*sumargb0*alpha.| B0*sumargb0*alpha.| xmm8
+        vmulps xmm5, xmm5, xmm9
+        vmulps xmm6, xmm6, xmm10
+        vmulps xmm7, xmm7, xmm11
+        vmulps xmm8, xmm8, xmm12
+        vpand xmm5, xmm5, xmm13
+        vpand xmm6, xmm6, xmm13
+        vpand xmm7, xmm7, xmm13
+        vpand xmm8, xmm8, xmm13
+
+
+        ; Cargamos el 1/max en xmm14 en fp
+        ; ->
+;|      1/MAX.       |      1/MAX.       |      1/MAX.       |      1/MAX.       | xmm14
+        vpxor xmm14, xmm14, xmm14
+        movq2dq xmm14, mm0
+        vmovddup xmm14, xmm14
 
         ; Dividir por MAX y sumarle el valor original de cada pixel
         ; ->
-;|       ldrR3.      |       ldrG3.      |       ldrB3.      |         0.        | xmm5
-;|       ldrR2.      |       ldrG2.      |       ldrB2.      |         0.        | xmm6
-;|       ldrR1.      |       ldrG1.      |       ldrB1.      |         0.        | xmm7
-;|       ldrR0.      |       ldrG0.      |       ldrB0.      |         0.        | xmm8
-        vfmadd132ps xmm5, xmm9, xmm14
-        vfmadd132ps xmm6, xmm10, xmm14
-        vfmadd132ps xmm7, xmm11, xmm14
-        vfmadd132ps xmm8, xmm12, xmm14
+;|        A3.        |       ldrR3.      |       ldrG3.      |       ldrB3.      | xmm5
+;|        A2.        |       ldrR2.      |       ldrG2.      |       ldrB2.      | xmm6
+;|        A1.        |       ldrR1.      |       ldrG1.      |       ldrB1.      | xmm7
+;|        A0.        |       ldrR0.      |       ldrG0.      |       ldrB0.      | xmm8
+        vmulps xmm5, xmm5, xmm14
+        vmulps xmm6, xmm6, xmm14
+        vmulps xmm7, xmm7, xmm14
+        vmulps xmm8, xmm8, xmm14
+        vaddps xmm5, xmm5, xmm9
+        vaddps xmm6, xmm6, xmm10
+        vaddps xmm7, xmm7, xmm11
+        vaddps xmm8, xmm8, xmm12
 
         ; Cargamos 255 para aplicar max/min
         ; ->
 ;|       255.        |       255.        |       255.        |       255.        | xmm13
+        vpxor xmm13, xmm13, xmm13
         movq2dq xmm13, mm1
-        movddup xmm13, xmm13
+        vmovddup xmm13, xmm13
 
         ; Aplicar min(max(xmmN,0.),255.)
-        maxps xmm5, xmm15
-        maxps xmm6, xmm15
-        maxps xmm7, xmm15
-        maxps xmm8, xmm15
-        minps xmm5, xmm13
-        minps xmm6, xmm13
-        minps xmm7, xmm13
-        minps xmm8, xmm13
+        vmaxps xmm5, xmm5, xmm15
+        vmaxps xmm6, xmm6, xmm15
+        vmaxps xmm7, xmm7, xmm15
+        vmaxps xmm8, xmm8, xmm15
+        vminps xmm5, xmm5, xmm13
+        vminps xmm6, xmm6, xmm13
+        vminps xmm7, xmm7, xmm13
+        vminps xmm8, xmm8, xmm13
 
         ; Convert the numbers back to integers
         ; ->
@@ -311,21 +300,22 @@ ldr_avx:
 ;|       ldrR2       |       ldrG2       |       ldrB2       |         0         | xmm6
 ;|       ldrR1       |       ldrG1       |       ldrB1       |         0         | xmm7
 ;|       ldrR0       |       ldrG0       |       ldrB0       |         0         | xmm8
-        cvtps2dq xmm5, xmm5
-        cvtps2dq xmm6, xmm6
-        cvtps2dq xmm7, xmm7
-        cvtps2dq xmm8, xmm8
+        vcvtps2dq xmm5, xmm5
+        vcvtps2dq xmm6, xmm6
+        vcvtps2dq xmm7, xmm7
+        vcvtps2dq xmm8, xmm8
 
         ; Pack the results into a single line again
         ; ->
-;|  0 | R3 | G3 | B3 |  0 | R2 | G2 | B2 |  0 | R1 | G1 | B1 |  0 | R0 | G0 | B0 | xmm8
-        packusdw xmm8, xmm7
-        packusdw xmm6, xmm5
-        packuswb xmm8, xmm6
-        psrld xmm8, 8
+;| A3 | R3 | G3 | B3 | A2 | R2 | G2 | B2 | A1 | R1 | G1 | B1 | A0 | R0 | G0 | B0 | xmm8
+        vpackusdw xmm8, xmm8, xmm7
+        vpackusdw xmm6, xmm6, xmm5
+        vpackuswb xmm8, xmm8, xmm6
+
+        .continue:
 
         ; Store in the destination
-        movdqu [rdi], xmm8
+        vmovdqu [rdi], xmm8
 
         add rsi, 16
         add rdi, 16
@@ -371,4 +361,5 @@ ldr_avx:
     pop rbx
     pop rbp
     ret
+
 
