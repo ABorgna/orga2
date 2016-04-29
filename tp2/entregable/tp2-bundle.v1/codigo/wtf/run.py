3aa634d18a1b8699397415f9a08d921acf0e9756
Agustin Borgna <hello@aborgna.com.ar>
2016-04-29 15:16:50 -0300

    borrar wtf

--- tp2/entregable/tp2-bundle.v1/codigo/wtf/run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-
-arguments = ['time', './tp2catedra', 'cropflip', '-t', '100000000',
-             '-i', 'c', './lena32.bmp', '--', '128 128 128 128']
-
-subprocess.check_call(arguments)
-
--- tp2/entregable/tp2-bundle.v1/codigo/wtf/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-time ./tp2catedra cropflip -t 10000 -i c ./lena32.bmp -- 128 128 128 128
