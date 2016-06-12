0f9bf58dffb4f851d31402b5c7b2aad6cbaef1de
gflan <twety.vg@gmail.com>
2016-06-11 23:44:26 -0300

    Reventar debugger en pantalla (falta llamarlo desde game y toda la bola)

--- tp3/src/screen.h
+++ tp3/src/screen.h
@@ -12,8 +12,16 @@
 #define VIDEO_FILS 50
 #define VIDEO_COLS 80
 
+#define DBG_COLS_INIT 20
+#define DBG_COLS_END 60
+
+#define DBG_FILS_INIT 7
+#define DBG_FILS_END 48
+
 #include "colors.h"
 #include "defines.h"
+#include "tasks/tss.h"
+
 /* Estructura de para acceder a memoria de video */
 typedef struct ca_s {
     unsigned char c;
@@ -29,5 +37,7 @@ void print_int(unsigned int n, unsigned int x, unsigned int y, unsigned char att
 void print_char(unsigned char c, unsigned int x, unsigned int y, unsigned char attr);
 
 void reventar_pantalla();
+
+void screen_show_debug(tss* tss, player_group group);
 
 #endif  /* !__SCREEN_H__ */
