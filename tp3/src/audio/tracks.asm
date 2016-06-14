48b9c57d5fd94e85f07115252d1848a57b708126
alan-corleto <alancorleto@hotmail.es>
2016-06-14 20:19:35 -0300

    Hubo que quitar cancioncitas por temas de memoria :(

--- tp3/src/audio/tracks.asm
+++ tp3/src/audio/tracks.asm
@@ -34,14 +34,14 @@ INCLUDE_AUDIO mario1, "audio/tracks/mario_1.audio"
 INCLUDE_AUDIO megaman0, "audio/tracks/megaman_0.audio"
 INCLUDE_AUDIO megaman1, "audio/tracks/megaman_1.audio"
 
-INCLUDE_AUDIO pokemon_gsc0, "audio/tracks/pokemon_gsc_0.audio"
-INCLUDE_AUDIO pokemon_gsc1, "audio/tracks/pokemon_gsc_1.audio"
+INCLUDE_AUDIO_CMT pokemon_gsc0, "audio/tracks/pokemon_gsc_0.audio"
+INCLUDE_AUDIO_CMT pokemon_gsc1, "audio/tracks/pokemon_gsc_1.audio"
 
-INCLUDE_AUDIO pokemon_rby0, "audio/tracks/pokemon_rby_0.audio"
-INCLUDE_AUDIO pokemon_rby1, "audio/tracks/pokemon_rby_1.audio"
+INCLUDE_AUDIO_CMT pokemon_rby0, "audio/tracks/pokemon_rby_0.audio"
+INCLUDE_AUDIO_CMT pokemon_rby1, "audio/tracks/pokemon_rby_1.audio"
 
 INCLUDE_AUDIO_CMT sonic0, "audio/tracks/sonic_0.audio"
 INCLUDE_AUDIO_CMT sonic1, "audio/tracks/sonic_1.audio"
 
-INCLUDE_AUDIO superfantasy0, "audio/tracks/superfantasy_0.audio"
-INCLUDE_AUDIO superfantasy1, "audio/tracks/superfantasy_1.audio"
+INCLUDE_AUDIO_CMT superfantasy0, "audio/tracks/superfantasy_0.audio"
+INCLUDE_AUDIO_CMT superfantasy1, "audio/tracks/superfantasy_1.audio"
